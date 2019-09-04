502413ebccd38ecab1406fce46f55b513a01bddb
xiaofeng1986 <chnathena@gmail.com>
2019-09-04 15:47:04 +0800

    remove unused class and target folder file

--- src/test/java/hellocucumber/RunCucumberTest.java
+++ src/test/java/hellocucumber/RunCucumberTest.java
@@ -6,7 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty","summary"}
+        plugin = {"html:target/cucumber-html-report",
+                "json:target/cucumber.json",
+                "pretty","summary"}
         , features = "src/test/resources/hellocucumber/image_recongnize_api.feature"
         , monochrome = true
 )
--- target/test-classes/hellocucumber/image_recongnize_api.feature
+++
@@ -1,7 +0,0 @@
-Feature: image analysis
-  SFA input API
-
-  Scenario: call image analysis successfully
-    Given prepare token
-    When call image analysis input API
-    Then image records generate in IR platform.
